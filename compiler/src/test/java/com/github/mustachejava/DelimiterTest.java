785ed445469c296148beeb26d52e0d2fec0f7164
DrRacket <zpy1995@gmail.com>
2017-12-24 12:53:05 +0000

    339161fc2bae replay gib_eks

--- compiler/src/test/java/com/github/mustachejava/DelimiterTest.java
+++ compiler/src/test/java/com/github/mustachejava/DelimiterTest.java
@@ -32,65 +32,6 @@ public class DelimiterTest {
     assertEquals("Hello, Jason.", sw.toString());
   }
 
-  @Test
-  public void testWithTemplateFunction() throws IOException {
-    DefaultMustacheFactory mf = new DefaultMustacheFactory();
-    Mustache maven = mf.compile(new StringReader("Hello, ${#f}${foo}${/f}."), "maven", "${", "}");
-    StringWriter sw = new StringWriter();
-    maven.execute(sw, new Object() {
-      TemplateFunction f = (s) -> s;
-      String foo = "Jason";
-    }).close();
-    assertEquals("Hello, Jason.", sw.toString());
-  }
-
-  @Test
-  public void testWithTemplateFunction2() throws IOException {
-    DefaultMustacheFactory mf = new DefaultMustacheFactory();
-    Mustache maven = mf.compile(new StringReader("Hello, ${foo}."), "maven", "${", "}");
-    StringWriter sw = new StringWriter();
-    maven.execute(sw, new Object() {
-      TemplateFunction foo = (s) -> "${name}";
-      String name = "Jason";
-    }).close();
-    assertEquals("Hello, ${name}.", sw.toString());
-  }
-
-  @Test
-  public void testStrSubstitutor() throws IOException {
-    DefaultMustacheFactory mf = new DefaultMustacheFactory();
-    Mustache maven = mf.compile(new StringReader("Hello, $<foo>."), "maven", "$<", ">");
-    StringWriter sw = new StringWriter();
-    maven.execute(sw, new Object() {
-      String foo = "Jason";
-    }).close();
-    assertEquals("Hello, Jason.", sw.toString());
-  }
-
-  @Test
-  public void testStrSubstitutor2() throws IOException {
-    DefaultMustacheFactory mf = new DefaultMustacheFactory();
-    Mustache maven = mf.compile(new StringReader("{{=$< >=}}Hello, $<foo>."), "maven");
-    StringWriter sw = new StringWriter();
-    maven.execute(sw, new Object() {
-      String foo = "Jason";
-    }).close();
-    assertEquals("Hello, Jason.", sw.toString());
-  }
-
-  @Test
-  public void testTemporarilyChangeDelimiters() throws IOException {
-    DefaultMustacheFactory mf = new DefaultMustacheFactory();
-    Mustache maven = mf.compile(new StringReader("AND {{tfield}} = {{=$< >=}}#{$<={{ }}=>{{name}}.{{{jfield}}}}"), "maven");
-    StringWriter sw = new StringWriter();
-    maven.execute(sw, new Object() {
-      String tfield = "someOtherId";
-      String name = "id";
-      String jfield = "1";
-    }).close();
-    assertEquals("AND someOtherId = #{id.1}", sw.toString());
-  }
-
   private static class NoEncodingMustacheFactory extends DefaultMustacheFactory {
     @Override
     public void encode(String value, Writer writer) {
